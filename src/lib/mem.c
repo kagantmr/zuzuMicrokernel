808d730b4b9908c8dc9303cc874565d29dc71a36
junk985 <kagantemur123@gmail.com>
2025-11-16 00:10:52 +0300

    Formatted printing implemented, boot text changed, mem.h header file filled.

--- src/lib/mem.c
+++ src/lib/mem.c
@@ -2,11 +2,15 @@
 
 
 void *memcpy(void *dest, const void *src, size_t n) {
-    
+
 };
 
 
-void *memset(void *ptr, char x, size_t n);
+void *memset(void *ptr, char x, size_t n) {
+    
+}
 
 
-void *memmove(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n) {
+
+}
